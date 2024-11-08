3be71e9a93acca5ee582376b816287b445175e22
izabelleharker <har23096@byui.edu>
2024-11-08 16:54:08 -0700

    commit

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,4 @@
-// Get the current year
+
 document.getElementById('currentyear').textContent = new Date().getFullYear();
 
-// Get the last modified date of the document
 document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
