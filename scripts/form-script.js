67d817c7bb52a8eaa435cf2d136435a916d67303
izabelleharker <har23096@byui.edu>
2024-12-06 01:19:01 -0700

    save

--- scripts/form-script.js
+++ scripts/form-script.js
@@ -1,4 +1,4 @@
-// Product Array
+
 const products = [
     { id: "fc-1888", name: "flux capacitor", averagerating: 4.5 },
     { id: "fc-2050", name: "power laces", averagerating: 4.7 },
@@ -7,11 +7,11 @@ const products = [
     { id: "jj-1969", name: "warp equalizer", averagerating: 5.0 }
   ];
   
-  // Populate the product selection options dynamically
+  
   window.onload = function() {
       const productSelect = document.getElementById('productName');
       
-      // Loop through the product array and create options
+      
       products.forEach(product => {
           const option = document.createElement('option');
           option.value = product.id;
@@ -19,12 +19,12 @@ const products = [
           productSelect.appendChild(option);
       });
   
-      // Check if the review counter is saved in localStorage
+      
       if (!localStorage.getItem('reviewCount')) {
           localStorage.setItem('reviewCount', 0);
       }
   
-      // Increment the review count when the page is loaded
+      
       localStorage.setItem('reviewCount', parseInt(localStorage.getItem('reviewCount')) + 1);
   };
   
